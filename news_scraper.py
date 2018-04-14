023cd57858884031a8f32cb9f768c30fa026125b
cat <cat@cat.centos>
2018-04-14 13:23:56 +0100

    moved headline scraper to function that returns a list

--- news_scraper.py
+++ news_scraper.py
@@ -2,16 +2,24 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
-bbc_news = 'http://www.bbc.co.uk'
 
-r = requests.get(bbc_news + '/news')
+def get_bbc_headlines():
+    """Return list of headlines from http://www.bbc.co.uk/news"""
+    headlines = []
 
-soup = BeautifulSoup(r.text, 'html.parser')
+    bbc_news = 'http://www.bbc.co.uk'
+    headline_class = 'gs-c-promo-heading nw-o-link-split__anchor gs-o-faux-block-link__overlay-link'
 
-for l in soup.find_all('a',
-                       {'class': re.compile('gs-c-promo-heading nw-o-link-split__anchor gs-o-faux-block-link__overlay-link*')}):
-    if l.h3:
-        if (l['href'][0:1]) == '/':
-            print(l.h3.string)
+    r = requests.get(bbc_news + '/news')
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    for l in soup.find_all('a', {'class': re.compile(headline_class + '*')}):
+        if l.h3:
+            if (l['href'][0:1]) == '/':
+                headlines.append(l.h3.string)
+
+    return headlines
 
 
+for h in get_bbc_headlines():
+    print(h)
